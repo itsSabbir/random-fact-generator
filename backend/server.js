1ebdd305832bf3e65d2c61cc053283051c58689f
itsSabbir <53710832+itsSabbir@users.noreply.github.com>
2023-10-03 17:04:00 -0400

    made some changes and testing out git

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// TODO: Setup middleware
-// TODO: Connect to database
+// Middleware to parse JSON requests
+app.use(express.json());
 
-// TODO: Setup routes
+// A little more than basic route for testing
+app.get('/', (req, res) => {
+    res.send(`
+        <h2>Welcome to the Random Fact Generator API!</h2>
+        <p>This API provides random facts. It's currently running and ready to serve requests.</p>
+        <button onclick="confirmFunction()">Check Status</button>
+        <script>
+            function confirmFunction() {
+                alert('Yup, it's still working!');
+            }
+        </script>
+    `);
+});
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
